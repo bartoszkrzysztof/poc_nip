ded1a9033b22dd90714b972b8f495e2585d5bd4f
Bartek-BUKOWSKI\Bartek <bukowski@mda.pl>
2017-08-30 21:31:43 +0200

    wczytywanie z histori

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
 function InputController ($scope, $http, $window, $filter) {
-	$scope.companyId = { text : '7835083242', number : '' };
+	$scope.companyId = { text : '', number : '' };
 	$scope.url = 'http://ihaveanidea.aveneo.pl/NIPAPI/api/Company';
 	
 	
 	function inputValue() {
 		var validatedId = new ValidateId($scope.companyId.text);
-		$scope.companyId.number = validatedId.num;
+		$scope.companyId.number = validatedId.prefix + validatedId.num;
 		$scope.companyId.type = validatedId.type;
+		$scope.companyId.valid = validatedId.valid;
 	};
 	
 	$scope.$watch('companyId.text', inputValue);
@@ -16,50 +17,58 @@ function InputController ($scope, $http, $window, $filter) {
 		$scope.storageData = [];
 	}
 	
-//	console.log(Object.keys($scope.storageData));
-	
-
-	
 	$scope.showInfo = function(){
-		
-		for (var i in $scope.storageData) {
-			if ($scope.storageData[i]['SearchResult']['CompanyId'] == $scope.companyId.number) {
-				var inHistory = true;
-				break;
+		if ($scope.companyId.valid === true) {
+			$scope.inHistory = false;
+			CheckInHistory($scope.storageData, $scope.companyId.number);
+
+			if ($scope.inHistory !== true) {
+				$http({
+					url: $scope.url,
+					method: 'GET',
+					params: {CompanyId: $scope.companyId.number}
+					}).then(function(response) {
+						$scope.companyInfo = response.data;
+
+						if ($scope.companyInfo.CompanyInformation !== null) {
+							var obj = {
+								SearchResult: {
+									CompanyId: $scope.companyId.number,
+									TimeStamp: Date.now(),
+									CompanyInformation: $scope.companyInfo.CompanyInformation
+								}
+							};
+							$scope.storageData.push(obj);
+
+							$window.localStorage.setItem('searchHistory', $filter('json')($scope.storageData, 1));
+						}
+				});
 			}
-		}
-		if (inHistory !== true) {
-			$http({
-				url: $scope.url,
-				method: 'GET',
-				params: {CompanyId: $scope.companyId.number}
-				}).then(function(response) {
-					$scope.companyInfo = response.data;
-
-					if ($scope.companyInfo.Success === true) {
-						var localStorage = $scope.storageData;
-
-						var obj = {
-							SearchResult: {
-								CompanyId: $scope.companyId.number,
-								TimeStamp: Date.now(),
-								CompanyInformation: $scope.companyInfo.CompanyInformation
-							}
-						};
-						localStorage.push(obj);
-
-						$window.localStorage.setItem('searchHistory', $filter('json')(localStorage, 1));
-					}
-			});
 		}
 
 	};
+	$scope.showPreviousInfo = function(id) {
+		CheckInHistory($scope.storageData, id);
+	}
+	
+	function CheckInHistory (storage, id) {
+		$scope.companyId.text = id;
+		for (var i in storage) {
+			if (storage[i]['SearchResult']['CompanyId'] == id) {
+				$scope.companyInfo = storage[i]['SearchResult'];
+				$scope.inHistory = true;
+				break;
+			}
+		}
+	}
 } 
 
 function ValidateId (string) {
 	var number = string.replace(/[^0-9]/g, ''),
 		strLenght = number.length,
-		type = '';
+		type = '',
+		prefix = '',
+		valid = false;
 	
 	if (strLenght < 9) { 
 		var type = 'numer za krótki'; 
@@ -68,15 +77,19 @@ function ValidateId (string) {
 		var validation = NipRegonCheck(number, '657234567'); 
 		if (validation == true) {
 			var type = 'nip';
+			var valid = true;
 		}
 		else {
 			var type = 'krs';
+			var valid = true;
+			var prefix = 'KRS'
 		}
 	}
 	else if (strLenght == 9 || strLenght == 14) { 
 		var validation = NipRegonCheck(number, '89234567'); 
 		if (validation == true) {
 			var type = 'regon';
+			var valid = true;
 		}
 		else {
 			var type = 'niepoprawny numer';
@@ -88,6 +101,8 @@ function ValidateId (string) {
 	
 	this.num = number;
 	this.type = type;
+	this.valid = valid;
+	this.prefix = prefix;
 }
 
 function NipRegonCheck(string, check) {
